bcf292e98a69cb8255b2645f62b82ac52aab0752
RodrigoJorge <rodrigozjorge@gmail.com>
2023-03-15 13:55:42 -0300

    api do google para qr code do whatsapp

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ let menu = document.querySelector(".menu");
 hamburger.addEventListener("click", function() {
   menu.classList.toggle("show");
 });
+
+var url = 'https://wa.me/5522992043536';
+var qr = 'https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=' + encodeURIComponent(url);
+document.getElementById('qrcode').src = qr;
