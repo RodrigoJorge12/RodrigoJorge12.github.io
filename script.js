e07ba147488412da0bcbf588131dcf04ce21f3b8
RodrigoJorge <rodrigozjorge@gmail.com>
2023-08-31 19:33:30 -0300

    feat: alteração nos projetos

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let hamburger = document.querySelector(".hamburger");
 let menu = document.querySelector(".menu");
+let botao = document.getElementById("botaoProjetoNaoConcluido");
+
+botao.addEventListener("click",function(){
+  alert("Projeto em andamento, em breve estara disponivel")
+})
 
 hamburger.addEventListener("click", function() {
   menu.classList.toggle("show");
