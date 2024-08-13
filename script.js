a8ada885ac849148dae8af30f74c2147d39fa0b9
Rodrigo Jorge <rodrigo@ssparisi.com.br>
2024-08-12 21:49:55 -0300

    alterações no site e configurando como um projeto npm

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-  let botao1 = document.getElementById("btn-projeto1");
-  let botao2 = document.getElementById("btn-projeto2");
-  let botao3 = document.getElementById("btn-projeto3");
 
 
-  botao1.addEventListener("click", function () {
-    alert("Projeto em andamento, em breve estara disponivel")
-  })
+  let botao1 = $("#btn-projeto1");
+  let botao2 = $("#btn-projeto2");
+  let botao3 = $("#btn-projeto3");
 
-  botao2.addEventListener("click", function () {
-    alert("Projeto em andamento, em breve estara disponivel")
-  })
 
-  botao3.addEventListener("click", function () {
-    alert("Projeto em andamento, em breve estara disponivel")
-  })
-})
+  botao1.click(function() {
+    alert("Projeto em andamento, em breve estará disponível");
+  });
+
+  botao2.click(function() {
+    alert("Projeto em andamento, em breve estará disponível");
+  });
+
+  botao3.click(function() {
+    alert("Projeto em andamento, em breve estará disponível");
+  });
+
+
+  let descricaoSobre = $("#descricaoSobre")
+
+  descricaoSobre.text("Nascido em Nova Friburgo, atualmente com 21 anos cursando sistemas de informação no CEFET-RJ no 7° periodo. Na empresa SS Parisi desde julho de 2023, tendo atuado de julho até dezembro como estagiario de desenvolvimento e de dezembro de 2023 até o momento como Desenvolvedor Web Junior utilizando principalmente as linguagens de programação Python, PHP e JavaScript juntamente com CSS, HTML, Jquery e Banco de Dados MySQL.")
+
 
 
